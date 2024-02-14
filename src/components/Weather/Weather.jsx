c38926d69a468c88d29be2bc2124d9fdf10eb0c8
Oscar Mumm <oscarmumm@gmail.com>
2024-02-14 12:48:12 -0300

    agregado icono de viento, corregido width de los tiles de pronostico

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -3,6 +3,7 @@ import CurrentWeather from "../CurrentWeather/CurrentWeather";
 import Next24hsForecast from "../Next24hsForecast/Next24hsForecast";
 import WindForecast from "../WindForecast/WindForecast";
 import temperature_icon from '../../assets/icons/thermometer.svg'
+import wind_2_icon from '../../assets/icons/wind_2.svg'
 
 const Weather = ({ currentWeatherData, forecastData }) => {
     return (
@@ -12,7 +13,7 @@ const Weather = ({ currentWeatherData, forecastData }) => {
             </div>
             <div className="weather-details">
                 <h4 className="weather-24hs-forecast-title">
-                    <span>Próximas 24hs</span>
+                    <span className="weather-24hs-forecast-title__main">Próximas 24hs</span>
                 </h4>
                 <h4 className="weather-24hs-forecast-title">
                     <img className="temperature-icon" src={temperature_icon} alt="" />
@@ -20,7 +21,8 @@ const Weather = ({ currentWeatherData, forecastData }) => {
                 </h4>
                 <Next24hsForecast forecastData={forecastData} />
                 <h4 className="weather-24hs-forecast-title">
-                    <span>Viento:</span>
+                    <img className="wind-icon" src={wind_2_icon} alt="" />
+                    <span>Viento</span>
                 </h4>
                 <WindForecast forecastData={forecastData} />
             </div>
