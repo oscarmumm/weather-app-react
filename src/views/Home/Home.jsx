3714b65ad361bd6229729256c61109df67ae260f
Oscar Mumm <oscarmumm@gmail.com>
2024-02-16 12:51:02 -0300

    agregado fondo de pantalla al home

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,25 +1,32 @@
-import weather_logo from '../../assets/icons/weather.svg'
-import './Home.css'
-import {motion} from 'framer-motion'
+import weather_logo from "../../assets/icons/weather.svg";
+import home_bg from '../../assets/img/home-bg.webp'
+import "./Home.css";
+import {motion} from "framer-motion";
 
 const homeVariants = {
-  hidden: {
-    opacity: 0,
-    y: 50
-  },
-  visible: {
-    opacity: 1,
-    y: 0
-  }
-}
+    hidden: {
+        opacity: 0,
+        y: 150,
+    },
+    visible: {
+        opacity: 1,
+        y: 0,
+    },
+};
 
 const Home = () => {
-  return (
-    <motion.div className='home-view-container' variants={homeVariants} initial='hidden' animate='visible'>
-        <img className='main-logo' src={weather_logo} alt="" />
-        <h1 className='main-title'>The Weather App</h1>
-    </motion.div>
-  )
-}
+    return (
+        <div className="home-view-container">
+            <img className="home-bg" src={home_bg} alt="" />
+            <motion.h1
+                variants={homeVariants}
+                initial="hidden"
+                animate="visible"
+                className="main-title">
+                The Weather App
+            </motion.h1>
+        </div>
+    );
+};
 
-export default Home
+export default Home;
